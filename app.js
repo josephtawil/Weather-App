5308ad8dba3df2db36b82b68b66e35b096a142d0
Joseph Tawil <jtawil2015@gmail.com>
2020-04-29 21:42:38 -0700

    created weather app

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var zipcode = "";
+    var apikey = "afbe116e2efffa373247065ba46d08b3";
+    var city = "San Francisco";
+    $("#button").on("click", function (e) {
+        e.preventDefault();
+        zipcode = $("#text").val();
+        $.ajax({
+            type: "GET",
+            //units would change the temperature from kelvin to fahrenheit
+            url: `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&id=${city}&units=imperial&appid=${apikey}`,
+            dataType: "json",
+        }).then(function (response) {
+            console.log(response.name);
+            kelvin = response.main.temp;
+            city = response.name;
+            // var fah = (kelvin - 273.15) * 9 / 5 + 32;
+            $("body").append(`<h1>The weather is ${kelvin} in ${city}</h1>`);
+        });
+    });
+
+});
